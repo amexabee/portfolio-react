fb07053a8d53b3ed1828cfa5c76f9370422e9ce1
Amanuel Galema <amexabee@yahoo.com>
2023-05-02 16:42:41 +0300

    Install @react-three/fiber and @react-three/drei

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,24 +1,33 @@
-import { useState } from 'react';
-import Footer from './footer';
-import NavBar from './navBar';
-import { Outlet } from 'react-router-dom';
+// import { useState } from 'react';
+// import Footer from './footer';
+// import NavBar from './navBar';
+// import { Outlet } from 'react-router-dom';
+
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+
 const Body = () => {
-  const [night, setNight] = useState(true);
+  // const [night, setNight] = useState(true);
 
-  const handleClick = () => {
-    let value = night ? false : true;
-    setNight(value);
-  };
+  // const handleClick = () => {
+  //   let value = night ? false : true;
+  //   setNight(value);
+  // };
 
-  const mode = night ? 'dark' : 'light';
+  // const mode = night ? 'dark' : 'light';
 
   return (
-    <div className="container">
-      <NavBar night={night} handleClick={handleClick} />
-      <Outlet context={[mode]} />
-      <Footer night={night} />
-    </div>
+    <Canvas>
+      <OrbitControls />
+      <Stars />
+    </Canvas>
   );
 };
 
 export default Body;
+
+// <div className="container">
+//   <NavBar night={night} handleClick={handleClick} />
+//   <Outlet context={[mode]} />
+//   <Footer night={night} />
+// </div>
